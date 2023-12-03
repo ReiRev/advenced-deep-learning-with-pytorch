628211581ef26d5bc759163f9fca9c4bd6742115
reirev <reirev2913@gmail.com>
2023-12-03 14:05:28 +0000

    Update devcontainer.json and add postCreateCommand.sh script

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing Python dependencies..."
+pip install --use-deprecated=legacy-resolver -r requirements.txt || true
+
+echo "Configuring Git..."
+git config --global --add safe.directory /workspace
+
+echo "Installing pre-commit hooks..."
+pre-commit install
+
+echo "Starting Jupyter notebook..."
+tmux new-session -d -s jupyter_session -n jupyter_window
+tmux send-keys -t jupyter_session:jupyter_window "jupyter notebook --no-browser --port=8080 --allow-root --NotebookApp.token='' --NotebookApp.password='' -y" Enter
