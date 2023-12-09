8d02a25cea60cbd744ac17bdf5591dfa4d436797
reirev <reirev2913@gmail.com>
2023-12-09 03:09:33 +0000

    Update devcontainer configuration and reinstall PyTorch

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,9 @@
 echo "Installing Python dependencies..."
 pip install --use-deprecated=legacy-resolver -r requirements.txt || true
 
+echo "Reinstalling PyTorch..."
+pip3 install numpy --pre torch torchvision torchaudio --force-reinstall --index-url https://download.pytorch.org/whl/nightly/cu118 || true
+
 echo "Configuring Git..."
 git config --global --add safe.directory /workspace
 
